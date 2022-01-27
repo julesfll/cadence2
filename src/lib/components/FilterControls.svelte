c4235399d28cac844399c654e855fe6b3ad4fac7
Jules Le Menestrel <juleslemenestrel@gmail.com>
2022-01-27 16:45:38 -0600

    Add reset button on slider

--- src/lib/components/FilterControls.svelte
+++ src/lib/components/FilterControls.svelte
@@ -4,15 +4,21 @@
 	import { createTempoFilter } from '$lib/utils';
 	import { trackFilter } from '$lib/stores';
 
-	// TODO: store tempoFilter(bpm[0], bpm[1], allowHafltime) in sveltestore
-	//      track display items can use this filter with fitlerBpm to show and hide tracks
-	let bpms = [170, 190];
+	const initialBpms = [170, 190];
+
+	let bpms = initialBpms;
 	let allowHalftime = true;
+
+	function resetSlider() {
+		bpms = initialBpms;
+		allowHalftime = true;
+	}
 
 	$: $trackFilter = createTempoFilter(bpms[0], bpms[1], allowHalftime);
 </script>
 
 <div class="p-2">
+	<button on:click={resetSlider}>R</button>
 	<RangeSlider
 		min={150}
 		max={200}
